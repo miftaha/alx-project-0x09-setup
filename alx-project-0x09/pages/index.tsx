f0e14cba429c7760eb69e6604216e116253351fb
Miftah <miftahaadem@gmail.com>
2025-01-14 21:44:07 +0300

    final commit

--- alx-project-0x09/pages/index.tsx
+++ alx-project-0x09/pages/index.tsx
@@ -1,7 +1,7 @@
 import ImageCard from '@/components/common/ImageCard'
 import { ImageProps } from '@/interfaces'
 import { useState } from 'react'
-
+//  last
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>('')
   const [imageUrl, setImageUrl] = useState<string>('')
@@ -16,7 +16,9 @@ const Home: React.FC = () => {
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
+        <h1 className="text-4xl font-bold mb-2 capitalize">
+          Image Generation App
+        </h1>
         <p className="text-lg text-gray-700 mb-4">
           Generate stunning images based on your prompts!
         </p>
